4a86aae7afc15e589f021ada936eb6f8d55a516a
serii <seriiburduja@gmail.com>
2024-03-13 17:44:59 +0200

    feat: home template finished

--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
 import HomeIntro from "../home/HomeIntro.vue";
+import PostsLoop from "../home/PostsLoop.vue";
+import Aside from "../home/Aside.vue";
 </script>
 
 <template>
   <HomeIntro/>
+  <div class="container">
+    <div class="row">
+      <!-- Blog entries-->
+      <div class="col-lg-8">
+        <PostsLoop/>
+      </div>
+      <div class="col-lg-4">
+        <Aside/>
+      </div>
+    </div>
+  </div>
 </template>
