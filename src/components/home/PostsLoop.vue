693d271f8d14039290e162b8f72682e16345a6c4
serii <seriiburduja@gmail.com>
2024-03-15 09:52:34 +0200

    feat: Preloader

--- src/components/home/PostsLoop.vue
+++ src/components/home/PostsLoop.vue
@@ -29,18 +29,4 @@ onMounted(() => {
       <PostItem v-for="post in posts_row[1]" :key="post.id" :post="post"/>
     </div>
   </div>
-  <!-- Pagination-->
-  <nav aria-label="Pagination">
-    <hr class="my-0"/>
-    <ul class="pagination justify-content-center my-4">
-      <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
-      <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
-      <li class="page-item"><a class="page-link" href="#!">2</a></li>
-      <li class="page-item"><a class="page-link" href="#!">3</a></li>
-      <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
-      <li class="page-item"><a class="page-link" href="#!">15</a></li>
-      <li class="page-item"><a class="page-link" href="#!">Older</a></li>
-    </ul>
-  </nav>
-
 </template>
--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -6,23 +6,35 @@ import {Ihome} from "../../interfaces/home/Ihome.ts";
 import {onMounted, ref} from "vue";
 import {axiosInstance} from "../../axios/axios-instance.ts";
 import {IPost} from "../../interfaces/home/IPost.ts";
+import Preloader from "../loading/Preloader.vue";
 
 const home = ref<Ihome>();
 const posts = ref<IPost[]>([]);
+const loading = ref<boolean>(true);
 
 async function getHome() {
+  loading.value = true;
   const data = await axiosInstance.get('/home');
   home.value = data.data;
   posts.value = home.value.posts;
+  stopLoading();
 }
 
 async function getPostsByCategory(id: number) {
+  loading.value = true;
   const data = await axiosInstance.get(`/post?category_id=${id}`);
   posts.value = data.data.posts;
+  stopLoading();
 }
 
 async function init() {
   await getHome();
+}
+
+function stopLoading() {
+  setTimeout(() => {
+    loading.value = false;
+  }, 500);
 }
 
 onMounted(() => {
@@ -32,12 +44,15 @@ onMounted(() => {
 
 <template>
   <HomeIntro/>
-  <div class="container">
+  <div class="container" style="min-height: 100vh">
     <div class="row" v-if="home">
       <!-- Blog entries-->
       <div class="col-lg-8">
-        <h3>Posts count: {{posts.length}}</h3>
-        <PostsLoop v-if="posts" :posts="posts"/>
+        <Preloader v-if="loading"/>
+        <div v-else>
+          <h3>Posts count: {{ posts.length }}</h3>
+          <PostsLoop v-if="posts" :posts="posts"/>
+        </div>
       </div>
       <div class="col-lg-4">
         <Aside v-if="home.categories" :categories="home.categories" @emit_category_id="getPostsByCategory"/>
