34fc948ad2d259b18d97348143f547ad33a5052d
serii <seriiburduja@gmail.com>
2024-03-19 10:16:17 +0200

    feat: login view

---
+++ src/components/views/LoginView.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {axiosInstance} from "../../axios/axios-instance";
+import ErrorMessage from "../message/ErrorMessage.vue";
+
+const email = ref("");
+const password = ref("");
+
+const errors = ref([]);
+
+const onSubmit = async () => {
+  try {
+    await axiosInstance.post('/login', {
+      email: email.value,
+      password: password.value,
+    });
+    errors.value = [];   //console.log(repsonse, "repsonse");
+  } catch (e: any) {
+    // console.log(e.response.status, "e.response.status");
+    if (e.response.status === 422 || e.response.status === 401) {
+      console.log(e.response.data, 'e')
+      if (e.response.data.errors) {
+        errors.value = e.response.data.errors;
+      }
+      // console.log(e.response.data.errors, 'response.data.errors')
+    } else {
+      console.log(e.response.data.message, "e.response.data.message");
+      console.log(e.response.data, 'e')
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="register">
+      <div style="min-height: 100vh;">
+        <div class="register__form">
+          <h2 class="mb-3">Login</h2>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" v-model="email" aria-describedby="emailHelp">
+            <ErrorMessage v-if="errors['email']" :error="errors['email'][0]" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" v-model="password">
+            <ErrorMessage v-if="errors['password']" :error="errors['password'][0]" />
+          </div>
+          <button @click="onSubmit" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import HomeView from "./components/views/HomeView.vue";
+import LoginView from "./components/views/LoginView.vue";
 import RegisterView from "./components/views/RegisterView.vue";
 
 export const routes = [
     {path: '/', component: HomeView},
     { path: '/register', component: RegisterView },
+    { path: '/login', component: LoginView },
     // { path: '/contact', component: Contact }
 ]
