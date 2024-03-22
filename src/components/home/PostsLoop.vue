8f1e9e353226ef9f82239a78957229f7e70b76bf
serii <seriiburduja@gmail.com>
2024-03-22 10:07:32 +0200

    feat: Posts filter pagination

--- src/components/home/PostsLoop.vue
+++ src/components/home/PostsLoop.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
+import VPagination from "@hennge/vue3-pagination";
+import "@hennge/vue3-pagination/dist/vue3-pagination.css";
 import {IPost} from "../../interfaces/home/IPost.ts";
 import PostItem from "../post/PostItem.vue";
 import {onMounted, ref} from "vue";
 import useChunkArray from "../../hooks/useChunkArray.ts";
 
+const emits = defineEmits(['emit_change_page']);
+
 const props = defineProps<{
-  posts: IPost[]
+  posts: IPost[],
+  current_page: number,
+  total_pages: number,
+  per_page: number
 }>();
 
-const posts_row = ref<array>([]);
+const posts_row = ref([]);
+const current_page = ref(props.current_page);
+
+function updateHandler(page: number) {
+  emits('emit_change_page', page);
+}
 
 
 onMounted(() => {
   posts_row.value = useChunkArray(props.posts, 2);
-  console.log(posts_row.value, 'posts_row.value')
+  // console.log(posts_row.value, 'posts_row.value')
 })
 </script>
 
@@ -21,10 +33,19 @@ onMounted(() => {
   <!-- Nested row for non-featured blog posts-->
   <div class="row">
     <div class="col-lg-6">
-      <PostItem v-for="post in posts_row[0]" :key="post.id" :post="post"/>
+      <PostItem v-for="post in posts_row[0]" :key="post.id" :post="post" />
     </div>
     <div class="col-lg-6">
-      <PostItem v-for="post in posts_row[1]" :key="post.id" :post="post"/>
+      <PostItem v-for="post in posts_row[1]" :key="post.id" :post="post" />
     </div>
   </div>
+  <div v-if="total_pages > 1" class="row mb-4">
+    <v-pagination v-model="current_page" :pages="total_pages" :range-size="1" active-color="#DCEDFF" @update:modelValue="updateHandler" />
+  </div>
+
 </template>
+<style lang="scss">
+.Pagination {
+  justify-content: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,6 @@
 import HomeIntro from "../components/home/HomeIntro.vue";
 import PostsLoop from "../components/home/PostsLoop.vue";
 import Aside from "../components/home/Aside.vue";
-import {Ihome} from "../interfaces/home/Ihome.ts";
 import {onMounted, ref} from "vue";
 import {axiosInstance} from "../axios/axios-instance.ts";
 import {IPost} from "../interfaces/home/IPost.ts";
@@ -11,19 +10,34 @@ import {ICategoryResponse} from "../interfaces/api/ICategoryResponse.ts";
 
 const posts = ref<IPost[]>([]);
 const categories = ref<ICategoryResponse[]>([]);
+const category_id = ref<number>(0);
 const loading = ref<boolean>(true);
+const post_current_page = ref<number>(1);
+const post_total_pages = ref<number>(1);
+const post_per_page = ref<number>(2);
 
-async function getPostsByCategory(id: number) {
+async function getPostsByCategory(id: number, page: number = 1) {
   loading.value = true;
-  const data = await axiosInstance.get(`/post?category_id=${id}`);
+  const data = await axiosInstance.get(`/post?category_id=${id}&page=${page}`);
   posts.value = data.data.data;
-  console.log(posts.value, "posts.value");
+  post_current_page.value = data.data.meta.current_page;
+  post_total_pages.value = data.data.meta.last_page;
+  post_per_page.value = data.data.meta.per_page;
   stopLoading();
 }
 
 async function getCategories() {
   const data = await axiosInstance.get("/category");
   categories.value = data.data.categories;
+}
+
+async function setCategoryId(id: number) {
+  category_id.value = id;
+  await getPostsByCategory(id);
+}
+
+async function setCurrentPage(page: number) {
+  await getPostsByCategory(category_id.value, page);
 }
 
 async function init() {
@@ -46,16 +60,16 @@ onMounted(() => {
   <HomeIntro />
   <div class="container" style="min-height: 100vh">
     <div class="row">
-      <!-- Blog entries-->
       <div class="col-lg-8">
         <Preloader v-if="loading" />
         <div v-else>
           <h3>Posts count: {{ posts.length }}</h3>
-          <PostsLoop v-if="posts" :posts="posts" />
+          <PostsLoop v-if="posts" @emit_change_page="setCurrentPage" :per_page="post_per_page"
+            :current_page="post_current_page" :total_pages="post_total_pages" :posts="posts" />
         </div>
       </div>
       <div class="col-lg-4">
-        <Aside v-if="categories" :categories="categories" @emit_category_id="getPostsByCategory" />
+        <Aside v-if="categories" :categories="categories" @emit_category_id="setCategoryId" />
       </div>
     </div>
   </div>
