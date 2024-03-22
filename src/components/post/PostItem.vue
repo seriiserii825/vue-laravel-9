c1273ffd68cc2b31cc91bcdd689ce367732b612f
serii <seriiburduja@gmail.com>
2024-03-22 09:25:20 +0200

    feat: Post item date

--- src/components/post/PostItem.vue
+++ src/components/post/PostItem.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
   <div class="card mb-4">
     <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..."/></a>
     <div class="card-body">
-      <div class="small text-muted">January 1, 2023</div>
+      <div class="small text-muted">{{post.created_at}}</div>
       <h2 class="card-title">{{post.title}}</h2>
       <p class="card-text">{{post.text}}</p>
       <a class="btn btn-primary" href="#!">Read more →</a>
