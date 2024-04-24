b076cf4333af05180a3f48ecc2588f0d7c6cdd04
serii <seriiburduja@gmail.com>
2024-04-24 16:19:37 +0300

    feat: sort field and direction

--- src/views/AdminPostsView.vue
+++ src/views/AdminPostsView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {onMounted, ref, watch} from 'vue';
+  import {onMounted, ref, watch} from 'vue';
 import {axiosInstance} from '../axios/axios-instance';
 import {IPost} from '../interfaces/home/IPost';
 import {ICategoryResponse} from '../interfaces/api/ICategoryResponse';
@@ -16,6 +16,8 @@ const loading = ref(true);
 const total = ref(0);
 const category_options = ref<ICategoryResponse[]>([]);
 const current_category = ref<ICategoryResponse>({});
+const sort_field = ref<string>('created_at')
+const sort_direction = ref<string>('desc')
 
 function setFalse() {
   setTimeout(() => {
@@ -26,9 +28,9 @@ function setFalse() {
 async function getPosts() {
   loading.value = true;
   try {
-    let url = `/admin/post?page=${current_page.value}&category_id=${current_category.value.id}`;
+    let url = `/admin/post?page=${current_page.value}&category_id=${current_category.value.id}&sort_field=${sort_field.value}&sort_direction=${sort_direction.value}`;
     if (current_category.value.id === 0) {
-      url = `/admin/post?page=${current_page.value}`;
+      url = `/admin/post?page=${current_page.value}&sort_field=${sort_field.value}&sort_direction=${sort_direction.value}`;
     }
     const response = await axiosInstance.get(url);
     posts.value = response.data.data;
@@ -68,9 +70,9 @@ async function updateCurrent(page: number) {
 watch(current_category, async (newVal) => {
   loading.value = true;
   try {
-    let url = `/admin/post?category_id=${newVal.id}`;
+    let url = `/admin/post?category_id=${newVal.id}&sort_field=${sort_field.value}&sort_direction=${sort_direction.value}`;
     if (newVal.id === 0) {
-      url = `/admin/post`;
+      url = `/admin/post?sort_field=${sort_field.value}&sort_direction=${sort_direction.value}`;
     }
     const response = await axiosInstance.get(url);
     posts.value = response.data.data;
@@ -83,6 +85,17 @@ watch(current_category, async (newVal) => {
     console.log(error, "error");
   }
 });
+
+async function sortField(field: string) {
+  if (sort_field.value === field) {
+    sort_direction.value = sort_direction.value === 'asc' ? 'desc' : 'asc';
+    await getPosts();
+  } else {
+    sort_field.value = field;
+    sort_direction.value = 'asc';
+    await getPosts();
+  }
+}
 
 function getCategoryName(categoryId: number) {
   const category = categories.value.find((category) => category.id === categoryId);
@@ -109,9 +122,21 @@ onMounted(() => {
       <thead>
         <tr>
           <td>Id</td>
-          <td>Title</td>
+          <td>
+            <a @click.prevent="sortField('title')" href="#">
+              Title
+              <span v-if="sort_field === 'title' && sort_direction === 'desc'">&uarr;</span>
+              <span v-if="sort_field === 'title' && sort_direction === 'asc'">&darr;</span>
+            </a>
+          </td>
           <td>Category</td>
-          <td>Created At</td>
+          <td>
+            <a @click.prevent="sortField('created_at')" href="#">
+              Created At
+              <span v-if="sort_field === 'created_at' && sort_direction === 'desc'">&uarr;</span>
+              <span v-if="sort_field === 'created_at' && sort_direction === 'asc'">&darr;</span>
+            </a>
+          </td>
         </tr>
       </thead>
       <tbody>
